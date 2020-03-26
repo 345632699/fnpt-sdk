520d74f6353a6d706b1fd1f9b57c213c6fff2d19
xu <34563269@qq.com>
2020-03-26 11:24:57 +0800

    蜂鸟跑腿 回调参数获取修正

--- src/main/java/com/wuyi/express/controller/NotifyController.java
+++ src/main/java/com/wuyi/express/controller/NotifyController.java
@@ -7,6 +7,7 @@ import com.wuyi.express.HttpRequestUtils;
 import com.wuyi.express.entity.Notify;
 import com.wuyi.express.fnpt.util.GsonUtil;
 import com.wuyi.express.service.NotifyService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -38,7 +39,7 @@ public class NotifyController {
     @PostMapping("add")
     public CommonResult add(@RequestBody Notify notify) {
         Notify byOrderNo = notifyService.getByOrderNo(notify.getOrderNo());
-        if (byOrderNo.getId() > 0) {
+        if (byOrderNo != null) {
             return CommonResult.failed("无需重复上报");
         }
         notifyService.saveOrUpdate(notify);
@@ -46,13 +47,14 @@ public class NotifyController {
     }
 
     @PostMapping("notify")
-    public Object notify(@RequestBody Map<String, Object> notify) throws Exception {
-        Map<String, Object> para = GsonUtil.GsonToMaps(notify.get("para").toString());
-        String orderNo = para.get("order_no").toString();
+    public Object notify(@RequestParam Map<String, String> notify) throws Exception {
+        String para = notify.get("para");
+        Map<String, Object> map = GsonUtil.GsonToMaps(para.replace("\\", ""));
+        String orderNo = map.get("order_no").toString();
 
         Notify byOrderNo = notifyService.getByOrderNo(orderNo);
 
-        String s = HttpRequestUtils.httpPost(byOrderNo.getNotifyUrl(), para);
+        String s = HttpRequestUtils.httpPost(byOrderNo.getNotifyUrl(), map);
         Map<String, Object> responseMap = GsonUtil.GsonToMaps(s);
         Map response = new HashMap();
         if (responseMap.get("code").equals(new Double("0"))) {
